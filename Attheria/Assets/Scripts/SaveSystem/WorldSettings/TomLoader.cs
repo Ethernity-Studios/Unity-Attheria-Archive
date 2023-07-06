ef3ed5711b82c64f6216df8241876e4ae4fb5efd
Golby <golbygames@gmail.com>
2023-07-06 13:40:27 +0200

    Update TomLoader.cs

--- Attheria/Assets/Scripts/SaveSystem/WorldSettings/TomLoader.cs
+++ Attheria/Assets/Scripts/SaveSystem/WorldSettings/TomLoader.cs
@@ -5,11 +5,11 @@ using Tommy;
 
 namespace SaveSystem.WorldSettings {
     public static class TomLoader {
-        static TomlNode writeObject(object obj, Type type) {
+        static TomlNode writeObject(object obj) {
             var node = new TomlTable();
 
-            foreach (var field in type.GetFields()) {
-                node[field.Name] = writeValue(field.GetValue(obj), field.FieldType);
+            foreach (var field in obj.GetType().GetFields()) {
+                node[field.Name] = writeValue(field.GetValue(obj));
             }
 
             return node;
@@ -75,7 +75,7 @@ namespace SaveSystem.WorldSettings {
             {
                 "Dictionary" => writeDictionary((Dictionary<string, object>)value, clazz.GenericTypeArguments[1]),
                 "List" => writeArray((IEnumerable<object>)value, clazz.GenericTypeArguments[0]),
-                _ => writeObject(value, clazz)
+                _ => writeObject(value)
             };
         }
         
